c17b5c6901020732f5e777d54ab9cad1e9c7c85f
insh007 <aliinshad9546@gmail.com>
2023-01-29 19:13:26 +0530

    upto fetching&Pagination

---
+++ src/components/NewsItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class NewsItem extends Component {
+
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props
+        return (
+            <div className='my-3'>
+                <div className="card" >
+                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{zIndex : '1', left: '85%'}}>
+                        {source}</span>
+                    <img src={imageUrl ? imageUrl : "https://images.moneycontrol.com/static-mcnews/2022/10/software-770x433.jpg"} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title.length > 44 ? title + "..." : title} <span className="badge rounded-pill bg-info text-dark">New</span></h5>
+                        <p className="card-text">{description.length > 87 ? description + "..." : description}</p>
+                        <p className="card-text"><small className="text-muted">By {author ? author : "someone"} on {new Date(date).toGMTString()}</small></p>
+                        <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
